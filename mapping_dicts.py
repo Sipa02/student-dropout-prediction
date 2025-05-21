30effe75cbd6327678ae7e790a45f38d843cb420
Syifa Azzahro <53552996+Sipa02@users.noreply.github.com>
2025-05-21 09:07:30 +0800

    Create mapping_dicts.py

---
+++ mapping_dicts.py
@@ -0,0 +1,19 @@
+# mapping_dict.py
+FIELD_MAPPINGS = {
+    "Gender": {"Male": 1, "Female": 0},
+    "Scholarship holder": {"Yes": 1, "No": 0},
+    "Tuition fees up to date": {"Yes": 1, "No": 0},
+    "International": {"Yes": 1, "No": 0},
+    "Debtor": {"Yes": 1, "No": 0},
+    "Marital status": {
+        "Single": 1,
+        "Married": 2,
+        "Widowed": 3,
+        "Divorced": 4,
+        "Other": 0
+    },
+    "Daytime/evening attendance\t": {
+        "Daytime": 1,
+        "Evening": 0
+    }
+}
